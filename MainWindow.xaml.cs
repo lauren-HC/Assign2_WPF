ce64c0151c7c5294fde3265b1e84b030301dcf98
Hyesoo Cho <hcho1@utas.edu.au>
2021-05-27 20:25:56 +1000

    new WPF project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignemt_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string RESEARCHER_LIST_KEY = "reasercherList";
+        private ResearcherController resCon;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            resCon = (ResearcherController)(Application.Current.FindResource(RESEARCHER_LIST_KEY) as ObjectDataProvider).ObjectInstance;
+        }
+
+        private void FilterDropBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.RemovedItems.Count > 0)
+            {
+                MessageBox.Show("Dropdown list used to select: " + e.AddedItems[0]);
+            }
+        }
+
+        private void ResearcherList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PublicationController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemt_2
+{
+    class PublicationController
+    {
+        public static List<Publication> loadPublications(Researcher researcher)
+        {
+            String name = researcher.GivenName + " " + researcher.FamilyName;
+            List<Publication> publications;
+
+            publications = DbAdaptor.fetchBasicPublicationDetails(name);
+
+            publications.Sort((p1, p2) => p1.Year.CompareTo(p2.Year));
+            publications.Reverse();
+
+            return publications;
+        }
+
+        public static Publication loadPublicationDetails(Publication publication)
+        {
+            return DbAdaptor.completePublicationDetails(publication);
+        }
+
+
+        public static List<Publication> filterByYearRange(List<Publication> publications, int startyear, int endyear)
+        {
+            for (int i = 0; i < publications.Count(); i++)
+            {
+                if (publications[i].Year < startyear || publications[i].Year > endyear)
+                {
+                    publications.Remove(publications[i]);
+                }
+            }
+
+            return publications;
+        }
+
+        /* public static List<Publication> invertSort(List<Publication> publications)
+         {
+             return publications.Reverse();
+         }*/
+
+        // returns a list of how many publications a researcher has published for a given start year finish
+        public static List<int[]> CumulativeCount(DateTime startDate, DateTime finishDate, int Id)
+        {
+
+            int count = 0; // how many publications for a particular year
+            int year = startDate.Year; // the year being counted
+
+            List<Publication> publications = DbAdaptor.fetchPublicaionCount(Id);
+
+
+            List<int[]> PubCount = new List<int[]>();
+
+            while (year <= finishDate.Year)
+            {
+                count = 0;
+                for (int i = 0; i < publications.Count; i++)
+                {
+                    if (publications[i].Year == year)
+                    {
+                        count++;
+                    }
+                }
+
+                int[] yearCount = { year, count };
+                PubCount.Add(yearCount);
+                year++;
+            }
+
+            return PubCount;
+        }
+    }
+}
